e66362cc5ece367dbef8df035cc3631c9a846205
Santiago Arias <sanargo97@gmail.com>
2023-06-03 16:06:17 -0500

    moving folders

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,7 @@
 function Cards(props) {
   return (
     <a
-      className="inline-block  h-60 w-60 hover:scale-105 ease-in-out duration-300 m-3 snap-start opacity-50 hover:opacity-100"
+      className="inline-block h-60 w-60 hover:scale-105 ease-in-out duration-300 m-3 snap-start opacity-50 hover:opacity-100"
       href={props.url}
       target="_blank"
     >
